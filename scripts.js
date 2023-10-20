284c35a6e3231b1895d87f5ffc10efa957e8e13d
ykaruus <147627870+ykaruus@users.noreply.github.com>
2023-10-20 11:00:14 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 const $menu = document.getElementById('menu');
 const $btn = document.getElementById('btn');
+const $btn_close = document.getElementById('btnClose')
 
 $btn.addEventListener('click', () => {
     if ($menu.style.left = '-450px') {
         $menu.style.left = '0px';
     }
+})
+
+$btn_close.addEventListener('click', () => {
+    $menu.style.left = '-450px'
 })
